d6de94a8a1e48232c623d0792aa10cd562b0c142
Eric Webber <ewebber283@gmail.com>
2021-10-16 18:50:11 -0500

    completed form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,25 @@ const tasksToDoEl = document.querySelector('#tasks-to-do');
 
 const createTaskHandler = function() {
     event.preventDefault();
+
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     const listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
-    listItemEl.textContent= 'new task';
-    tasksToDoEl.appendChild(listItemEl);
+
+    // create div to hold task info and add to list item
+    const taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    taskInfoEl.innerHTML ="<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    //add div to li
+    listItemEl.appendChild(taskInfoEl);
    
+    //add li to ul
+    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
 };
 
 formEl.addEventListener('submit', createTaskHandler);
